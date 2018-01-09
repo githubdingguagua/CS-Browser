86e6aa29ce09ad9fef16289d6908cc18b0afa0f9
milos-mkv <milosh.mkv@gmail.com>
2018-01-09 16:30:29 +0100

    First add

---
+++ Browser/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace Browser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.materialRaisedButton5 = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.materialRaisedButton6 = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.materialRaisedButton7 = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.materialRaisedButton8 = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.materialSingleLineTextField2 = new MaterialSkin.Controls.MaterialSingleLineTextField();
+            this.materialRaisedButton9 = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.materialTabControl1 = new MaterialSkin.Controls.MaterialTabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.materialTabSelector1 = new MaterialSkin.Controls.MaterialTabSelector();
+            this.materialTabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // materialRaisedButton5
+            // 
+            this.materialRaisedButton5.Depth = 0;
+            this.materialRaisedButton5.Location = new System.Drawing.Point(12, 33);
+            this.materialRaisedButton5.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialRaisedButton5.Name = "materialRaisedButton5";
+            this.materialRaisedButton5.Primary = true;
+            this.materialRaisedButton5.Size = new System.Drawing.Size(35, 23);
+            this.materialRaisedButton5.TabIndex = 0;
+            this.materialRaisedButton5.Text = "<";
+            this.materialRaisedButton5.UseVisualStyleBackColor = true;
+            this.materialRaisedButton5.Click += new System.EventHandler(this.materialRaisedButton5_Click);
+            // 
+            // materialRaisedButton6
+            // 
+            this.materialRaisedButton6.Depth = 0;
+            this.materialRaisedButton6.Location = new System.Drawing.Point(53, 33);
+            this.materialRaisedButton6.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialRaisedButton6.Name = "materialRaisedButton6";
+            this.materialRaisedButton6.Primary = true;
+            this.materialRaisedButton6.Size = new System.Drawing.Size(35, 23);
+            this.materialRaisedButton6.TabIndex = 1;
+            this.materialRaisedButton6.Text = ">";
+            this.materialRaisedButton6.UseVisualStyleBackColor = true;
+            this.materialRaisedButton6.Click += new System.EventHandler(this.materialRaisedButton6_Click);
+            // 
+            // materialRaisedButton7
+            // 
+            this.materialRaisedButton7.Depth = 0;
+            this.materialRaisedButton7.Location = new System.Drawing.Point(94, 33);
+            this.materialRaisedButton7.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialRaisedButton7.Name = "materialRaisedButton7";
+            this.materialRaisedButton7.Primary = true;
+            this.materialRaisedButton7.Size = new System.Drawing.Size(75, 23);
+            this.materialRaisedButton7.TabIndex = 2;
+            this.materialRaisedButton7.Text = "Refresh";
+            this.materialRaisedButton7.UseVisualStyleBackColor = true;
+            this.materialRaisedButton7.Click += new System.EventHandler(this.materialRaisedButton7_Click);
+            // 
+            // materialRaisedButton8
+            // 
+            this.materialRaisedButton8.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.materialRaisedButton8.Depth = 0;
+            this.materialRaisedButton8.Location = new System.Drawing.Point(543, 33);
+            this.materialRaisedButton8.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialRaisedButton8.Name = "materialRaisedButton8";
+            this.materialRaisedButton8.Primary = true;
+            this.materialRaisedButton8.Size = new System.Drawing.Size(47, 23);
+            this.materialRaisedButton8.TabIndex = 3;
+            this.materialRaisedButton8.Text = "Go";
+            this.materialRaisedButton8.UseVisualStyleBackColor = true;
+            this.materialRaisedButton8.Click += new System.EventHandler(this.materialRaisedButton8_Click);
+            // 
+            // materialSingleLineTextField2
+            // 
+            this.materialSingleLineTextField2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.materialSingleLineTextField2.Depth = 0;
+            this.materialSingleLineTextField2.Hint = "";
+            this.materialSingleLineTextField2.Location = new System.Drawing.Point(175, 33);
+            this.materialSingleLineTextField2.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialSingleLineTextField2.Name = "materialSingleLineTextField2";
+            this.materialSingleLineTextField2.PasswordChar = '\0';
+            this.materialSingleLineTextField2.SelectedText = "";
+            this.materialSingleLineTextField2.SelectionLength = 0;
+            this.materialSingleLineTextField2.SelectionStart = 0;
+            this.materialSingleLineTextField2.Size = new System.Drawing.Size(362, 23);
+            this.materialSingleLineTextField2.TabIndex = 4;
+            this.materialSingleLineTextField2.UseSystemPasswordChar = false;
+            this.materialSingleLineTextField2.Click += new System.EventHandler(this.materialSingleLineTextField2_Click);
+            // 
+            // materialRaisedButton9
+            // 
+            this.materialRaisedButton9.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.materialRaisedButton9.Depth = 0;
+            this.materialRaisedButton9.Location = new System.Drawing.Point(596, 33);
+            this.materialRaisedButton9.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialRaisedButton9.Name = "materialRaisedButton9";
+            this.materialRaisedButton9.Primary = true;
+            this.materialRaisedButton9.Size = new System.Drawing.Size(53, 23);
+            this.materialRaisedButton9.TabIndex = 6;
+            this.materialRaisedButton9.Text = "Tab";
+            this.materialRaisedButton9.UseVisualStyleBackColor = true;
+            this.materialRaisedButton9.Click += new System.EventHandler(this.materialRaisedButton9_Click);
+            // 
+            // materialTabControl1
+            // 
+            this.materialTabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.materialTabControl1.Controls.Add(this.tabPage1);
+            this.materialTabControl1.Depth = 0;
+            this.materialTabControl1.Location = new System.Drawing.Point(0, 89);
+            this.materialTabControl1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialTabControl1.Name = "materialTabControl1";
+            this.materialTabControl1.SelectedIndex = 0;
+            this.materialTabControl1.Size = new System.Drawing.Size(662, 281);
+            this.materialTabControl1.TabIndex = 7;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(654, 255);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // materialTabSelector1
+            // 
+            this.materialTabSelector1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.materialTabSelector1.BaseTabControl = this.materialTabControl1;
+            this.materialTabSelector1.Depth = 0;
+            this.materialTabSelector1.Location = new System.Drawing.Point(0, 62);
+            this.materialTabSelector1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialTabSelector1.Name = "materialTabSelector1";
+            this.materialTabSelector1.Size = new System.Drawing.Size(662, 28);
+            this.materialTabSelector1.TabIndex = 8;
+            this.materialTabSelector1.Text = "materialTabSelector1";
+            // 
+            // Form1
+            // 
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(661, 373);
+            this.Controls.Add(this.materialTabSelector1);
+            this.Controls.Add(this.materialTabControl1);
+            this.Controls.Add(this.materialRaisedButton9);
+            this.Controls.Add(this.materialSingleLineTextField2);
+            this.Controls.Add(this.materialRaisedButton8);
+            this.Controls.Add(this.materialRaisedButton7);
+            this.Controls.Add(this.materialRaisedButton6);
+            this.Controls.Add(this.materialRaisedButton5);
+            this.MinimumSize = new System.Drawing.Size(534, 373);
+            this.Name = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.materialTabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MaterialSkin.Controls.MaterialRaisedButton materialRaisedButton1;
+        private MaterialSkin.Controls.MaterialRaisedButton materialRaisedButton2;
+        private MaterialSkin.Controls.MaterialRaisedButton materialRaisedButton3;
+        private MaterialSkin.Controls.MaterialSingleLineTextField materialSingleLineTextField1;
+        private MaterialSkin.Controls.MaterialRaisedButton materialRaisedButton4;
+        private System.Windows.Forms.Panel panel1;
+        private MaterialSkin.Controls.MaterialRaisedButton materialRaisedButton5;
+        private MaterialSkin.Controls.MaterialRaisedButton materialRaisedButton6;
+        private MaterialSkin.Controls.MaterialRaisedButton materialRaisedButton7;
+        private MaterialSkin.Controls.MaterialRaisedButton materialRaisedButton8;
+        private MaterialSkin.Controls.MaterialSingleLineTextField materialSingleLineTextField2;
+        private MaterialSkin.Controls.MaterialRaisedButton materialRaisedButton9;
+        private MaterialSkin.Controls.MaterialTabControl materialTabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private MaterialSkin.Controls.MaterialTabSelector materialTabSelector1;
+    }
+}
+
---
+++ Browser/Form1.cs
@@ -0,0 +1,119 @@
+﻿using CefSharp;
+using CefSharp.WinForms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Browser
+{
+    public partial class Form1 : MaterialSkin.Controls.MaterialForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+            
+        private void materialRaisedButton2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CefSettings settings = new CefSettings();
+            Cef.Initialize(settings);
+            materialSingleLineTextField2.Text = "https://www.google.com";
+            ChromiumWebBrowser chrome = new ChromiumWebBrowser(materialSingleLineTextField2.Text);
+            chrome.Parent = materialTabControl1.SelectedTab;
+            chrome.Dock = DockStyle.Fill;
+            chrome.AddressChanged += chrome_AddressChanged;
+            chrome.TitleChanged += chrome_TitleChanged;
+        }
+
+        void chrome_AddressChanged(object sender, AddressChangedEventArgs e)
+        {
+            this.Invoke(new MethodInvoker(() => { materialTabControl1.Text = e.Address; }));
+        }
+
+        private void materialRaisedButton8_Click(object sender, EventArgs e)
+        {
+            ChromiumWebBrowser chrome = materialTabControl1.SelectedTab.Controls[0] as ChromiumWebBrowser;
+            if(chrome != null)
+                chrome.Load(materialSingleLineTextField2.Text);
+        }
+
+        private void materialRaisedButton7_Click(object sender, EventArgs e)
+        {
+            ChromiumWebBrowser chrome = materialTabControl1.SelectedTab.Controls[0] as ChromiumWebBrowser;
+            if (chrome != null)
+                chrome.Refresh();
+        }
+
+        private void materialRaisedButton6_Click(object sender, EventArgs e)
+        {
+            ChromiumWebBrowser chrome = materialTabControl1.SelectedTab.Controls[0] as ChromiumWebBrowser;
+            if (chrome != null) {
+                if (chrome.CanGoForward)
+                {
+                    chrome.Forward();
+                }
+            }
+        }
+
+        private void materialRaisedButton5_Click(object sender, EventArgs e)
+        {
+            ChromiumWebBrowser chrome = materialTabControl1.SelectedTab.Controls[0] as ChromiumWebBrowser;
+             if (chrome != null)
+             {
+                 if (chrome.CanGoBack)
+                 {
+                     chrome.Back();
+                 }
+             }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Cef.Shutdown();
+        }
+
+        private void materialSingleLineTextField2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void materialRaisedButton9_Click(object sender, EventArgs e)
+        {
+            TabPage tab = new TabPage();
+            tab.Text = "New tab";
+            materialTabControl1.Controls.Add(tab);
+            materialTabControl1.SelectTab(materialTabControl1.TabCount - 1);
+            ChromiumWebBrowser chrome = new ChromiumWebBrowser("https://www.google.com");
+            chrome.Parent = tab;
+            chrome.Dock = DockStyle.Fill;
+            materialSingleLineTextField2.Text = "https://www.google.com";
+            chrome.AddressChanged += chrome_AddressChanged;
+            chrome.TitleChanged += chrome_TitleChanged;
+        }
+
+        void chrome_TitleChanged(object sender, TitleChangedEventArgs e)
+        {
+            this.Invoke(new MethodInvoker(() => { materialTabControl1.SelectedTab.Text = e.Title; }));
+        }
+
+        private void materialTabSelector1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+
+    }
+}
